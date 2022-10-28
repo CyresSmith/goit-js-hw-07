39b2e69e3225f46f5c2ceddb17f669032472215d
CyresSmith <107186155+CyresSmith@users.noreply.github.com>
2022-10-28 22:24:57 +0300

    done

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -5,23 +5,48 @@ const refs = {
   gallery: document.querySelector('.gallery'),
 }
 
-function createGalleryMarkup(items) {
+// ========== Создание и рендер разметки ==========
+
+const createGalleryMarkup = (items) => {
   const markup = items.map(({ preview, original, description }) => {
     return `<div class="gallery__item"><a class="gallery__link" href="${original}"><img class="gallery__image" src="${preview}" data-source="${original}" alt="${description}"/></a></div>`
   }).join('');
+
   return markup;
-}
+};
 
 const galleryMarkup = createGalleryMarkup(galleryItems);
 
 refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
 
-function onGalleryClick(e) {
+// ========== Модальное окно basicLightbox ==========
+
+let instance;
+
+const onGalleryClick = (e) => {
   event.preventDefault();
+
+  const imgSource = e.target.dataset.source;
+
   if (!e.target.classList.contains('gallery__image')) {
     return;
-  } 
-  basicLightbox.create(`<img width="1400" height="900" src="${e.target.dataset.source}">`).show();
-}
+  }
+
+  instance = basicLightbox.create(`<img width="1400" height="900" src="${imgSource}">`);
+
+  instance.show();
+  addWindowEventListener();
+};
 
 refs.gallery.addEventListener('click', onGalleryClick);
+
+// ========== Функции закрытия по Esc ==========
+
+const addWindowEventListener = () => window.addEventListener('keydown', onEscPress);
+
+const removeWindowEventListener = () => window.removeEventListener('keydown', onEscPress);
+
+const onEscPress = () => {
+  instance.close();
+  removeWindowEventListener();
+};
--- js/02-lightbox.js
+++ js/02-lightbox.js
@@ -1,4 +1,29 @@
 import { galleryItems } from './gallery-items.js';
 // Change code below this line
 
-console.log(galleryItems);
+const refs = {
+  gallery: document.querySelector('.gallery'),
+};
+
+// ========== Создание и рендер разметки ==========
+
+const createGalleryMarkup = (items) => {
+  const markup = items.map(({ preview, original, description }) => `<a class="gallery__item" href="${original}">
+                <img class="gallery__image" src="${preview}" alt="${description}"/>
+              </a>`).join('');
+
+  return markup;
+};
+
+const galleryMarkup = createGalleryMarkup(galleryItems);
+
+refs.gallery.insertAdjacentHTML('beforeend', galleryMarkup);
+
+// ========== Галерея SimpleLightbox ==========
+
+const lightbox = new SimpleLightbox('.gallery a', {
+  captionsData: 'alt',
+  captionDelay: 250,
+  showCounter: false,
+  disableRightClick: true,
+});
